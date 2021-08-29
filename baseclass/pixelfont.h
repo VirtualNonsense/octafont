cdcecece9e3c6177edddcfff5c5fa9b2db867a9d
Andreas Nachtmann <virtualnonsense@mailbox.org>
2021-08-29 22:42:31 +0200

    first working version

--- baseclass/pixelfont.h
+++ baseclass/pixelfont.h
@@ -1,57 +1,82 @@
-#ifndef PIXELFONT_H
-#define PIXELFONT_H
+//
+// Created by anachtmann on 8/23/21.
+//
+#ifndef FLIPDOT_PIXELFONT_H
+#define FLIPDOT_PIXELFONT_H
+
+#include <cstdint>
+#include <cstdlib>
+
+struct letter {
+    int height;
+    int width;
+    int start_index;
+    const bool *letter;
+};
 
 
 class PixelFont {
 
 public:
-    virtual ~PixelFont() { }
+    virtual ~PixelFont() = default;
 
     bool has_char(char c) {
         return get_jumps()[(int) c] != -1;
     }
 
-    uint8_t get_width(char c) {
+    int get_width(char c) {
         return get_widths()[(int) c];
     };
 
-    uint8_t get_octet(char c, uint8_t index) {
-        size_t jump_index = get_jumps()[(int) c];
+    int get_height() {
+        return get_font_height();
+    };
 
-        if (index >= get_width(c)) {
-            return 0x00;
+    int get_letter(char c, letter *letter) {
+        Serial.print("input: ");
+        Serial.println((int) c);
+        if (!has_char(c)) {
+            return -1;
         }
-
-        return get_chars()[jump_index + index];
+        letter->start_index = get_jumps()[(int) c];
+        letter->letter = get_chars();
+        letter->height = get_height();
+        letter->width = get_width(c);
+        return 0;
     }
 
-    static uint8_t get_undercut(PixelFont& font1, char c1, PixelFont& font2, char c2) {
-        if (!font1.has_char(c1) || !(font2.has_char(c2))) {
-            return 0;
-        }
-
-        uint8_t c1_width = font1.get_width(c1);
-        uint8_t octet1 = font1.get_octet(c1, c1_width - 1);
-        uint8_t octet2 = font2.get_octet(c2, 0);
-
-        if (octet1 & octet2) {
-            return 0;
-        }
-        if (octet1 & (octet2 >> 1)) {
-            return 0;
-        }
-        if (octet1 & (octet2 << 1)) {
-            return 0;
-        }
-
-        return 1;
+    static uint8_t get_undercut(PixelFont &font1, char c1, PixelFont &font2, char c2) {
+//        if (!font1.has_char(c1) || !(font2.has_char(c2))) {
+//            return 0;
+//        }
+//
+//        uint8_t c1_width = font1.get_width(c1);
+//        uint8_t octet1 = font1.get_letter(c1, c1_width - 1);
+//        uint8_t octet2 = font2.get_letter(c2, 0);
+//
+//        if (octet1 & octet2) {
+//            return 0;
+//        }
+//        if (octet1 & (octet2 >> 1)) {
+//            return 0;
+//        }
+//        if (octet1 & (octet2 << 1)) {
+//            return 0;
+//        }
+//
+//        return 1;
+        return 0;
     }
 
 private:
-    virtual const uint8_t* get_chars() = 0;
-    virtual const int* get_jumps() = 0;
-    virtual const int* get_widths() = 0;
+    virtual const bool *get_chars() = 0;
+
+    virtual const int *get_widths() = 0;
+
+    virtual const int *get_jumps() = 0;
+
+    virtual int get_font_height() = 0;
 
 };
 
-#endif //PIXELFONT_H
+#endif //FLIPDOT_PIXELFONT_H
--- mkfont.py
+++ mkfont.py
@@ -1,21 +1,21 @@
+from typing import *
 from PIL import Image
 from bitarray import bitarray
 from os import listdir, makedirs
 from os.path import realpath, dirname, join, isfile, basename, splitext, exists
 
 
-
-def extract_pixel_data(img, marker_line):
+def get_letter_borders(img, marker_line):
     result = []
     inside_character = False
     for i in range(0, img.width):
         p = img.getpixel((i, marker_line))
 
-        if p == (255, 0, 0):
+        if p == (255, 0, 0) or p == (255, 0, 0, 255):
             if not inside_character:
                 inside_character = True
                 start = i
-        elif p == (0, 255, 0):
+        elif p == (0, 255, 0) or p == (0, 255, 0, 255):
             if inside_character:
                 result.append((start, i))
                 inside_character = False
@@ -25,9 +25,8 @@ def extract_pixel_data(img, marker_line):
     return result
 
 
-def build_tables(img, bounds, y_pos):
+def old_build_tables(img, bounds, y_pos):
     jumps, widths, chars = [], [], []
-
     cursor = 0
     counter = 0
     for i in range(0, 256):
@@ -35,8 +34,8 @@ def build_tables(img, bounds, y_pos):
         if table_index != -1:
             (start, end) = bounds[table_index]
             width = end - start + 1
-            jumps.append(("\n    " if i % 16 == 0 and i else "") + str(cursor).rjust(5))
-            widths.append(("\n    " if i % 16 == 0 and i else "") + str(width).rjust(2))
+            jumps.append((f"\n/*{chr(i)}*/" if i % 8 == 0 and i else "") + str(cursor).rjust(5))
+            widths.append((f"\n/*{chr(i)}*/" if i % 8 == 0 and i else "") + str(width).rjust(2))
 
             for x in range(start, end + 1):
                 bits = bitarray(endian="little")
@@ -69,112 +68,158 @@ def build_tables(img, bounds, y_pos):
     return jumps_str, widths_str, chars_str
 
 
+def build_tables(img: Image.Image, bounds, y_pos, true_c: str, false_c: str):
+    jumps, widths, chars = [], [], []
+    # line zero is for guide
+    height = img.height - 1
+    cursor = 0
+    counter = 0
+    jump = 0
+    for i in range(0, 256):
+        table_index = get_char_mapping(i)
+        if table_index != -1:
+            (start, end) = bounds[table_index]
+            width = end - start + 1
+            jumps.append((f"\n/*{chr(i)}*/" if i % 8 == 0 and i else "") + str(jump).rjust(5))
+            widths.append((f"\n/*{chr(i)}*/" if i % 8 == 0 and i else "") + str(width).rjust(2))
+            jump += width * height
+
+            letter = []
+            comment = "/* " + chr(i) + " */\n"
+            for y in range(y_pos, y_pos + height):
+                row = []
+                for x in range(start, end + 1):
+                    p = img.getpixel((x, y))
+                    if p == (0, 0, 0) or p == (0, 0, 0, 255):
+                        entry = true_c
+                    else:
+                        entry = false_c
+                    if y == y_pos and x == start:
+                        row.append(comment + entry)
+                    else:
+                        row.append(entry)
+                letter.append(f"{', '.join(row)}")
+            chars.append(",\n".join(letter))
+        else:
+            jumps.append(("\n    " if i % 8 == 0 and i else "") + str(-1).rjust(5))
+            widths.append(("\n    " if i % 8 == 0 and i else "") + str(-1).rjust(2))
+    jumps_str = ", ".join(jumps)
+    widths_str = ", ".join(widths)
+    chars_str = ", \n".join(chars)
+    return jumps_str, widths_str, chars_str, height
+
+
 # ISO-8859-15
 def get_char_mapping(v):
     # Special supported characters from upper block
-    if v == 0xc4: return 94  # Ä
-    if v == 0xd6: return 95  # Ö
-    if v == 0xdc: return 96  # Ü
-    if v == 0xe4: return 97  # ä
-    if v == 0xf6: return 98  # ö
-    if v == 0xdf: return 99  # ß
-    if v == 0xfc: return 100 # ü
-    if v == 0xa4: return 101 # €
+    # if v == 0xc4: return 94  # Ä
+    # if v == 0xd6: return 95  # Ö
+    # if v == 0xdc: return 96  # Ü
+    # if v == 0xe4: return 97  # ä
+    # if v == 0xf6: return 98  # ö
+    # if v == 0xdf: return 99  # ß
+    # if v == 0xfc: return 100  # ü
+    # if v == 0xa4: return 101  # €
 
     # ASCII block (non control chars up to 127)
-    if 33 <= v <= 126:
-        return v - 33
+    if 32 <= v <= 126:
+        return v - 32
 
     # No mapping
     return -1
 
 
-def print_char(img, bounds, variant, v):
-    index = get_char_mapping(0xdf)
-
-    if index != -1:
-        (start, end) = bounds[index]
-
-        for y in range(variant["y_pos"], variant["y_pos"] + 8):
-            for x in range(start, end + 1):
-                p = img.getpixel((x, y))
-                if p == (0, 0, 0):
-                    print("x", end='')
-                else:
-                    print(" ", end='')
-            print("")
-    else:
-        print("No character data.")
+#
+# def print_char(img, bounds, variant):
+#     index = get_char_mapping(0xdf)
+#
+#     if index != -1:
+#         (start, end) = bounds[index]
+#
+#         for y in range(variant["y_pos"], variant["y_pos"] + 8):
+#             for x in range(start, end + 1):
+#                 p = img.getpixel((x, y))
+#                 if p == (0, 0, 0):
+#                     print("x", end='')
+#                 else:
+#                     print(" ", end='')
+#             print("")
+#     else:
+#         print("No character data.")
 
 
 if __name__ == '__main__':
     here = dirname(realpath(__file__))
-    input_dir = join(here, "pixeldata")
+    input_dir = join(here, "input")
     input_files = [join(input_dir, f) for f in listdir(input_dir) if
                    isfile(join(input_dir, f)) and f.lower().endswith(".png")]
 
+    define_true = "x"
+    define_false = "o"
     if not exists('output'):
         makedirs('output')
 
     for input_file in input_files:
-
         img = Image.open(input_file)
         font_name = splitext(basename(input_file))[0].capitalize()
 
-        variants = [
-            {"name": "Regular", "marker_line": 9, "y_pos": 10},
-            {"name": "Bold", "marker_line": 0, "y_pos": 1}
-        ]
+        bounds = get_letter_borders(img, 0)
 
-        for v in variants:
-            bounds = extract_pixel_data(img, v["marker_line"])
-            variant = v["name"]
-            (jumps, widths, chars) = build_tables(img, bounds, v["y_pos"])
+        jumps, widths, chars, height = build_tables(img, bounds, 1, define_true, define_false)
 
-            #print_char(img, bounds, v, 0xdf)
+        # print_char(img, bounds, v, 0xdf)
 
-            h_file = f"""#ifndef {font_name.upper()}_{variant.upper()}_H_
-        #define {font_name.upper()}_{variant.upper()}_H_
-        
-        #include "pixelfont.h"
-        
-        class {font_name}{variant} : public PixelFont {{
-        
-        private:
-            static const uint8_t chars[];
-            static const int jumps[];
-            static const int widths[];
-        
-            const uint8_t* get_chars() override {{
-                return chars;
-            }}
-        
-            const int* get_jumps() override {{
-                return jumps;
-            }}
-        
-            const int* get_widths() override {{
-                return widths;
-            }}
-        }};
-        
-        const uint8_t {font_name}{variant}::chars[] = {{
-        {chars}
-        }};
-        
-        const int {font_name}{variant}::jumps[] = {{
-        {jumps}
-        }};
-        
-        const int {font_name}{variant}::widths[] = {{
-        {widths}
-        }};
-        
-        #endif //{font_name.upper()}_{variant.upper()}_H_
-        """
-            output_file = join(here, "output", font_name.lower() + f"-{variant.lower()}.h")
-            print(basename(input_file) + " -> " + basename(output_file))
+        h_file = f"""
+#ifndef {font_name.upper()}_H_
+#define {font_name.upper()}_H_
 
-            f = open(output_file, "w")
-            f.write(h_file)
-            f.close()
+#include "pixelfont.h"
+
+class {font_name}: public PixelFont {{
+
+private:
+    static const bool chars[];
+    static const int jumps[];
+    static const int widths[];
+    static const int height = {height};
+
+    const bool *get_chars() override {{
+        return chars;
+    }}
+
+    const int* get_jumps() override {{
+        return jumps;
+    }}
+
+    const int* get_widths() override {{
+        return widths;
+    }}
+    
+    int get_font_height() override {{
+        return height;
+    }}
+}};
+
+#define {define_true} true
+#define {define_false} false
+const bool {font_name}::chars[] = {{
+{chars}
+}};
+#undef {define_true}
+#undef {define_false}
+
+const int {font_name}::jumps[] = {{
+{jumps}
+}};
+
+const int {font_name}::widths[] = {{
+{widths}
+}};
+#endif //{font_name.upper()}_H_
+"""
+        output_file = join(here, "output", font_name + ".h")
+        print(basename(input_file) + " -> " + basename(output_file))
+
+        f = open(output_file, "w")
+        f.write(h_file)
+        f.close()
